8f90fdc41b20a2bb88fea394a90484c5671c66b6
Dominik Guzy <dom.guzy@gmail.com>
2022-02-24 13:51:56 +0100

    chore: change ts config

--- packages/@gorrion/eslint-config/index.js
+++ packages/@gorrion/eslint-config/index.js
@@ -1,13 +1,3 @@
-const fs = require("fs");
-const path = require("path");
-
-const tsConfig = fs.existsSync("tsconfig.json")
-  ? path.resolve("tsconfig.json")
-  : fs.existsSync("types/tsconfig.json")
-  ? path.resolve("types/tsconfig.json")
-  : // eslint-disable-next-line no-undefined
-    undefined;
-
 module.exports = {
   env: {
     browser: true,
@@ -262,7 +252,6 @@ module.exports = {
       parser: "@typescript-eslint/parser",
       parserOptions: {
         ecmaVersion: 2018,
-        project: tsConfig,
         sourceType: "module",
       },
       plugins: ["@typescript-eslint"],
